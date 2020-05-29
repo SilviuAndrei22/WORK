6478d43d885f91e726e1714e5d18ca09399fa307
SilviuAndrei22 <nsilviuandrei@gmail.com>
2020-05-29 16:24:51 +0300

    Ubuntu Terminal Commit

---
+++ sample.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//struct User{
+//char User_Name[50];
+//int Sudo_Ability;
+//};
+
+int main() {
+
+//int i, k;
+//printf("Intended number of users:");
+//canf("%d", &k);
+//for(i=0; i<k;i++) {
+ //struct User U_i;
+ //scanf("%s", &U_i.User_Name);
+  //if (U_i.User_Name == "Admin") 
+  // U_i.Sudo_Ability = 1;
+  //else
+  // U_i.Sudo_Ability = 0;
+   //}
+printf("Work In Progres!\n");   
+
+
+
+return 0;
+}
+
